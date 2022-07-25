20b7827c079bca8b33efdf68907394318c51d529
Blagica Bozhinovska <blagicastojanoska@gmail.com>
2022-07-25 13:05:48 +0200

    Init commit

---
+++ src/config.ts
@@ -0,0 +1,37 @@
+import type{ NavItems } from './types'
+
+export const NAV_ITEMS: NavItems = {
+    // home: {
+    //     path: '/',
+    //     title: 'home'
+    // },
+    // blog: {
+    //     path: '/blog',
+    //     title: 'blog'
+    // },
+    // tags: {
+    //     path: '/tags',
+    //     title: 'tags'
+    // },
+    // media: {
+    //     path: '/media',
+    //     title: 'media'
+    // },
+    about: {
+        path: '/',
+        title: 'about'
+    }
+}
+
+export const SITE = {
+    // Your site's detail?
+    name: 'Blagica Bozhinovska',
+    title: 'Blagica Bozhinovska Portfolio',
+    description: 'Full-Stack Web Developer',
+    url: 'https://blagica.online',
+    githubUrl: 'https://github.com/BStojanoska',
+    linkedInUrl: 'https://www.linkedin.com/in/blagica-bozhinovska/',
+    listDrafts: true
+}
+
+export const PAGE_SIZE = 8
